21c86e1105b93201b89ebb41bdd6bb4af976af42
songsuhyeon <ssh1692@naver.com>
2023-01-02 07:03:22 +0900

    Js: Home Transparent

--- main.js
+++ main.js
@@ -34,6 +34,19 @@ contactBtn.addEventListener('click', () => {
     scrollIntoView('#contact')
 })
 
+// Make home slowly fade to ransparent as the  widow scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    // console.log(1 - window.scrollY / homeHeight)
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+
+
+
+
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior: 'smooth' });

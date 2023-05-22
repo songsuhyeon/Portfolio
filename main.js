bfda48fabf6a36710ef00e11e9919d3f2463ebda
송수현 <ssh1692@naver.com>
2023-05-22 15:58:46 +0900

    modified content

--- main.js
+++ main.js
@@ -75,17 +75,17 @@ workBtnContainer.addEventListener("click", (e) => {
 
   // Remove selection from the previous item and select the new one
   const active = document.querySelector(".category__btn.selected");
-  active.classList.remove("selected");
-  const target =
-    e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
+  if (active != null) {
+    active.classList.remove("selected");
+  }
   e.target.classList.add("selected");
 
   projectContainer.classList.add("anim-out");
   setTimeout(() => {
     projects.forEach((project) => {
-      console.log(project);
+      console.log(project.dataset.type);
       if (filter === "*" || filter === project.dataset.type) {
-        project.classList.remove("visible");
+        project.classList.remove("invisible");
       } else {
         project.classList.add("invisible");
       }
